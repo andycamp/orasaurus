46efb59e041d4b7438df69669060bb03ab567a8e
pmacydna@gmail.com <pmacydna@gmail.com>
2011-11-14 13:39:44 -0500

    Added sort first option.

--- lib/orasaurus/build.rb
+++ lib/orasaurus/build.rb
@@ -2,8 +2,15 @@ module Orasaurus
   
   module Build
     
-    def self.sort(arr,*opts)
-      return arr.sort
+    def self.sort(arr,opts={})
+      firsts = Array.new
+      if tmp = Array.try_convert(opts[:first])
+        firsts = opts[:first].to_ary
+      elsif tmp = String.try_convert(opts[:first])
+        firsts = Array.new.push(opts[:first])
+      end
+      
+      return ( firsts + ( arr - firsts ).sort ).compact
     end
     
   end
--- spec/build_spec.rb
+++ spec/build_spec.rb
@@ -3,9 +3,16 @@ require 'spec_helper'
 describe Orasaurus::Build do
 
   describe "#sort" do
+    
     it "should default to alphabetic." do
       Orasaurus::Build.sort(["z","y","x"]).should eql ["x","y","z"]
     end
+    
+    it "should be able to always put something first" do
+      Orasaurus::Build.sort(["z","y","x"], :first => "z" ).should eql ["z","x","y"]
+      Orasaurus::Build.sort(["z","y","x","w"], :first => ["z", "w"] ).should eql ["z","w","x","y"]
+    end
+    
   end
 
 end
