28a3e6fa24bbea0b608388d2d232be6f4bc2e6c3
Andy Campbell <pmacydna@gmail.com>
2011-11-20 17:13:23 -0500

    Adding a little dialog for the db connect params.

--- lib/orasaurus/cli.rb
+++ lib/orasaurus/cli.rb
@@ -7,18 +7,6 @@ module Orasaurus
 
   class CLI < Thor
     
-    module Helpers
-    
-      def process_db_connect_params(db_name, db_user, db_password)
-        params = Hash.new
-        params[:db_name] = db_name||ask("Database Name? ") { |q| q.echo = true }	  
-        params[:db_user] = db_user||ask("Database User? ") { |q| q.echo = true }
-        params[:db_password] = db_password||ask("Database Password? ") { |q| q.echo = true }
-        return params
-      end
-
-    end
-
     include Thor::Actions
 
     map "-v" => :version
@@ -37,7 +25,8 @@ module Orasaurus
       
       if options.sort_method.upcase == "SQL" then
         puts "connecting for sql sorting."
-        a.connect(options.db_username, options.db_password,options.db_name)
+        db_connect_options = process_db_connect_options(options.db_username, options.db_password, options,db_name)
+        a.connect(db_connect_options[:db_username], db_connect_options[:db_password],db_connect_options[:db_name])
         sort_options = { :method => :SQL, :db_connection => a.connection }
       else
         sort_options = {}  
@@ -54,42 +43,20 @@ module Orasaurus
       end
       
     end
-
+    
     desc "version", "Currently running version of Orasaurus."
     def version
       puts "Orasarus v"+Orasaurus::VERSION
     end
+    
+    def process_db_connect_options(db_username, db_password, db_name)
+       params = Hash.new
+       params[:db_name] = db_name||ask("Database Name? ") { |q| q.echo = true }	  
+       params[:db_username] = db_username||ask("Database User? ") { |q| q.echo = true }
+       params[:db_password] = db_password||ask("Database Password? ") { |q| q.echo = "*" }
+       return params
+     end
    
   end
-=begin  
-  class Generate < Thor::Group 
-    argument :script_type, :type => :string, :desc => "Use build, teardown, or all."
-    desc "generate scripts", "generate sqlplus scripts."   
-    
-    def generate(script_type)
-      
-      
-      
-      build_script_name = options[:build_script_name]||"build.sql"
-      teardown_script_name = options[:teardown_script_name]||"teardown.sql"
-
-      say "generating build scripts"
-      s = Orasaurus.new( '.' )
-      s.build_all_scripts( build_script_name, teardown_script_name )    
-    end
-
-    desc "generate_build_scripts", "generate sqlplus build scripts for the current directory."   
-    method_options :build_script_name => :string, :teardown_script_name => :string 
-    def teardown_scripts
-      build_script_name = options[:build_script_name]||"build.sql"
-      teardown_script_name = options[:teardown_script_name]||"teardown.sql"
-
-      say "generating build scripts"
-      s = Orasaurus::ScriptBuilder.new( '.' )
-      s.build_all_scripts( build_script_name, teardown_script_name )    
-    end
-    
-  end
-=end
 
 end
--- lib/orasaurus/version.rb
+++ lib/orasaurus/version.rb
@@ -1,3 +1,3 @@
 module Orasaurus
-  VERSION = "0.0.6.rc1"
+  VERSION = "0.0.6.rc2"
 end
